6d5ad542ba314b77de313919e64e7b744a453036
Olajide Balogun <olajidebalogun@icloud.com>
2023-11-12 00:34:33 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -3,108 +3,9 @@ import Navigation from './components/Navigation/Navigation';
 import Logo  from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import ParticlesBg from 'particles-bg';
+import axios from 'axios';
 import './App.css';
-
-const ParticleBackground = () => {
-  const particlesInit = useCallback(async engine => {
-      console.log(engine);
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async container => {
-      await console.log(container);
-  }, []);
-
-  return (
-    <div> 
-      <Particles
-           className= 'particles'
-           id="tsparticles"
-           init={particlesInit}
-           loaded={particlesLoaded}
-           options={{
-               background: {
-                   color: {
-                       value: "#0000000",
-                   },
-               },
-               fpsLimit: 60,
-               interactivity: {
-                   events: {
-                       onClick: {
-                           enable: true,
-                           mode: "push",
-                       },
-                       onHover: {
-                           enable: true,
-                           mode: "repulse",
-                       },
-                       resize: true,
-                   },
-                   modes: {
-                       push: {
-                           quantity: 4,
-                       },
-                       repulse: {
-                           distance: 200,
-                           duration: 0.4,
-                       },
-                   },
-               },
-               particles: {
-                   color: {
-                       value: "#ffffff",
-                   },
-                   links: {
-                       color: "#ffffff",
-                       distance: 150,
-                       enable: true,
-                       opacity: 0.5,
-                       width: 1,
-                   },
-                   collisions: {
-                       enable: true,
-                   },
-                   move: {
-                           directions: "none",
-                           enable: true,
-                           outModes: {
-                               default: "bounce",
-                           },
-                           random: false,
-                           speed: 3,
-                           straight: false,
-                       },
-                   number: {
-                       density: {
-                           enable: true,
-                           area: 800,
-                       },
-                       value: 80,
-                   },
-                   opacity: {
-                       value: 0.5,
-                   },
-                   shape: {
-                       type: "circle",
-                   },
-                   size: {
-                       value: { min: 1, max: 5 },
-                   },
-               },
-               detectRetina: true,
-           }}
-       />
-    </div>
-   );
-}
-
 
 
 class App extends Component {
@@ -117,11 +18,38 @@ class App extends Component {
    
   onInputChange = (event) => {
     console.log(event.target.value);
-  }
+   };
 
    onButtonSubmit = () => {
-    console.log('click');
-   }
+    const apiKey = 'e5a1a47cf9224ee6936e503ce77d1a5d';
+    const imageUrl = this.state.input;
+  
+    axios.post('https://api.clarifai.com/v2/models/{MODEL_ID}/outputs', {
+      inputs: [
+        {
+          data: {
+            image: {
+              url: imageUrl,
+            },
+          },
+        },
+      ],
+    }, {
+      headers: {
+        'Authorization': `Key ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      // Handle the response data
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+  }
+  
   render() {    
   return (
     
@@ -130,7 +58,7 @@ class App extends Component {
       <Logo />
       <Rank /> 
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <ParticleBackground />  
+      <ParticlesBg   type="circle" bg={true}/> 
      {/* <FaceRecognition /> */}
     </div>
   );
